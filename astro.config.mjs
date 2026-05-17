5961337a6918604248352233b07f207b22d384de
Yassine Jemlaoui <czentovic50@gmail.com>
2026-05-17 22:05:45 +0200

    js modication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  // If deploying to a sub-path (e.g. github.io/repo-name), set base:
-  // base: '/repo-name',
+  site: 'https://glikoaop0.github.io',
 });
